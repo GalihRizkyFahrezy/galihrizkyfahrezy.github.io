fe18686566bdfa2b892494d77060c15d355e90e7
GalihRizkyFahrezy <157792999+GalihRizkyFahrezy@users.noreply.github.com>
2024-06-03 20:20:20 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const element1 = document.getElementById("topvideo");
+const element2 = document.getElementById("topdiv");
+const element3 = document.getElementById("div01");
+
+document.getElementById("headerTop").onclick = function()
+{
+    element1.scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("headerTentang").onclick = function()
+{
+    element2.scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById("headerKemampuan").onclick = function()
+{
+    element3.scrollIntoView({behavior: "smooth"},true);
+}
